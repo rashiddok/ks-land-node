7149fe37a233606c6a95c157067d32f7dc27b3bc
escettitt <tekillermd@gmail.com>
2022-04-13 11:17:24 +0300

    feature: file logger added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import * as winston from 'winston';
+import { WinstonModule } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: WinstonModule.createLogger({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          filename: 'logs/debug.log', //name of file where will be saved logging result
+          level: 'debug',
+        }),
+        new winston.transports.File({
+          filename: 'logs/info.log',
+          level: 'info',
+        }),
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+        }),
+      ],
+    })
+  });
   app.enableCors();
   app.use(morgan('tiny'));
   await app.listen(3000);
